89178d9bf96f23b376b96cca5b3ff9aa6f9d8f49
kirtan73 <kirtan.khatana73@outlook.com>
2016-09-01 19:44:34 +0530

    Version 5 sucsessfully created with with more controls

---
+++ WebApplication1/WebApplication1/Kirtan_CE60/Version5.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Kirtan_CE60
+{
+    public partial class Version5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == true)
+            {
+                Panel1.BackColor = System.Drawing.Color.FromName(TextBox1.Text);
+                Label7.ForeColor = System.Drawing.Color.FromName(TextBox2.Text);
+            }
+
+        }
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownList2.SelectedIndex == 0)
+            {
+                TextBox2.Enabled = false;
+            }
+            else
+            {
+                TextBox2.Enabled = true;
+            }
+            Panel1.BorderStyle = (BorderStyle)(Int32.Parse(DropDownList2.SelectedItem.Value));
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            Label7.Text = TextBox3.Text;
+        }
+
+        protected void DropDownList3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Label7.Font.Name = DropDownList3.SelectedItem.Text;
+        }
+
+        protected void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            Label7.Font.Size = Int32.Parse(TextBox4.Text) + 8;
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(CheckBox1.Checked)
+            {
+                im1.Visible = true;
+ 
+            }
+            else
+            {
+                im1.Visible = false;
+            }
+        }
+
+
+    
+    
+    
+    }
+}
